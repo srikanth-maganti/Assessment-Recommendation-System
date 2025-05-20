0282ec94b07ab4adaaff818f304746f694471f16
srikanth-maganti <magantisrikanth45@gmail.com>
2025-05-20 18:21:53 +0530

    removed local model

--- tasks/assessment_api.py
+++ tasks/assessment_api.py
@@ -14,7 +14,7 @@ pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index = pc.Index(INDEX_NAME)
 
 
-embedder = SentenceTransformer('./local_model')
+embedder =SentenceTransformer('all-MiniLM-L6-v2')
 
 
 def embed_text(text):
